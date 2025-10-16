68d2c0a02f5a58c73c3aec96e074a2fac081d1a5
Jonny Jackson <64557905+jonnyjackson26@users.noreply.github.com>
2025-10-16 18:11:17 +0000

    fix error

--- main.py
+++ main.py
@@ -12,33 +12,25 @@ def readData(filepath):
 
 def generateMatrix(x,y,gap_cost,mismatch_cost):
     matrix = [[0] * (len(x)+1) for _ in range(len(y)+1)] #initalize 2d array with all 0s. #it's one bigger because we have an extra starting row/col for the base cases that are just adding mismatch cost each time
-    #generate first row and column as the letters
-    """
-    for i in range(len(x)):
-        matrix[0][i+1]=x[i:i+1]
-    for i in range(len(y)):
-        matrix[i+1][0]=y[i:i+1]
-    """
 
     #gen first row and col:
     for i in range(len(x)):
-        matrix[0][i+1]=matrix[0][i]-mismatch_cost
+        matrix[0][i+1]=matrix[0][i]-gap_cost
     for i in range(len(y)):
-        matrix[i+1][0]=matrix[i][0]-mismatch_cost
+        matrix[i+1][0]=matrix[i][0]-gap_cost
 
     #fill in matrix
     for i in range(1,len(matrix)):
         for j in range(1,len(matrix[i])):
             #decide what number to put in
-            x_character=x[i-1:i]
-            y_character=y[i-1:i]
-            #print(f"x: {x_character}, y: {y_character}")
+            x_char = x[j-1]
+            y_char = y[i-1]
 
-            diagonal=matrix[i-1][j-1]+1 #match #assuming 1 is the reward for matching characters
-            gap_on_x=matrix[i][j-1]+gap_cost    #pull down 
-            gap_on_y=matrix[i-1][j]+gap_cost    #pull down 
+            diagonal = matrix[i-1][j-1] + (1 if x_char == y_char else -mismatch_cost) #match #assuming 1 is the reward for matching characters
+            gap_on_x=matrix[i][j-1]-gap_cost    #pull down 
+            gap_on_y=matrix[i-1][j]-gap_cost    #pull down 
 
-            matrix[i][j]=min(diagonal, gap_on_x, gap_on_y)
+            matrix[i][j]=max(diagonal, gap_on_x, gap_on_y)
 
 
             
@@ -54,7 +46,7 @@ def printMatrix(matrix):
 
 #main:
 x,y,gap_cost,mismatch_cost=readData('data2.txt')
-print(x,y,gap_cost,mismatch_cost)
+print(f"Gap cost: {gap_cost}\nMismatch cost: {mismatch_cost}\nx: {x}\ny: {y}\n")
 
 matrix=generateMatrix(x,y,gap_cost,mismatch_cost)
 printMatrix(matrix)
